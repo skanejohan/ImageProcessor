5b307996af88337d0535e0464c10619d94d7ae6c
skanejohan <johan_m_ahlgren@hotmail.com>
2023-10-08 15:29:56 +0200

    Introduce WithPalette extension method on Image

--- ImageProcessorLib/ExtensionsInternal.cs
+++ ImageProcessorLib/ExtensionsInternal.cs
@@ -12,5 +12,18 @@
                     : PixelAlphaCompositionMode.SrcOver
             });
         }
+
+        public static int SquaredEuclideanDistance(this Rgba32 color, Rgba32 other)
+        {
+            if (color == other)
+            {
+                return 0;
+            }
+            var dr = color.R - other.R;
+            var dg = color.G - other.G;
+            var db = color.B - other.B;
+            var da = color.A - other.A;
+            return dr * dr + dg * dg + db * db + da * da;
+        }
     }
 }
--- ImageProcessorLib/ExtensionsPublic.cs
+++ ImageProcessorLib/ExtensionsPublic.cs
@@ -26,5 +26,45 @@
             });
             return clonedImage;
         }
+
+        public static Image WithPalette(this Image image, IEnumerable<Rgba32> palette)
+        {
+            Dictionary<Rgba32, Rgba32> closestColors = new();
+
+            var clonedImage = image.CloneAs<Rgba32>();
+            clonedImage.ProcessPixelRows(accessor =>
+            {
+                for (int y = 0; y < accessor.Height; y++)
+                {
+                    Span<Rgba32> pixelRow = accessor.GetRowSpan(y);
+                    foreach (ref Rgba32 pixel in pixelRow)
+                    {
+                        pixel = GetTargetColor(pixel);
+                    }
+                }
+            });
+            return clonedImage;
+
+            Rgba32 GetTargetColor(Rgba32 sourceColor)
+            {
+                if (closestColors.TryGetValue(sourceColor, out var closestColor))
+                {
+                    return closestColor;
+                }
+
+                var minDist = int.MaxValue;
+                foreach(var paletteColor in palette)
+                {
+                    var dist = sourceColor.SquaredEuclideanDistance(paletteColor);
+                    if (dist < minDist)
+                    {
+                        minDist = dist;
+                        closestColor = paletteColor;
+                    }
+                }
+                closestColors[sourceColor] = closestColor;
+                return closestColor;
+            }
+        }
     }
 }
