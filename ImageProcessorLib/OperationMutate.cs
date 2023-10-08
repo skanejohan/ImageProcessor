03bd801f8e9e2ba63ca52081d8af3fea2b7f8526
skanejohan <johan_m_ahlgren@hotmail.com>
2023-10-08 14:39:17 +0200

    Introduce general mutation operation

---
+++ ImageProcessorLib/OperationMutate.cs
@@ -0,0 +1,14 @@
+﻿namespace ImageProcessorLib
+{
+    public static class OperationMutate
+    {
+        /// <summary>
+        /// Perform a general operation by supplying the underlying action on IImageProcessingContext.
+        /// </summary>
+        public static ImageProcessor WithMutation(this ImageProcessor imageProcessor, Action<IImageProcessingContext> action)
+        {
+            imageProcessor.AddOperation(action);
+            return imageProcessor;
+        }
+    }
+}
